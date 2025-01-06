3141b7b6b68351c4cbcc7e8f6d26bec19233201c
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2025-01-06 14:54:19 +0100

    Added cache for static files

--- app/app.py
+++ app/app.py
@@ -95,7 +95,7 @@ def index():
         # Check if static page is requested
         if "static" in request.args:
             path = request.args["static"]
-            return send_file( safe_path(path,sroot) )
+            return send_file( safe_path(path,sroot),cache=True )
 
         # Else show the root directory
         proto = request.headers.get('X-Forwarded-Proto',request.scheme)
